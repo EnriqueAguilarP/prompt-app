d90141c28c3642554487a9332b3709b165fd374a
Jesus Aguilar <enrique.prado207@gmail.com>
2023-05-17 11:05:29 -0600

    Add header to Homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,19 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI Art Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+      Welcome to AI Art Prompt, the website where you can find and share some prompts for stunning artworks in seconds using the power of Artificial Intelligence. 
+      Whether you want to turn your photo into a masterpiece, generate an artwork from nothing but words, or combine concepts, attributes, and styles in novel ways, 
+      AI Art Prompt has you covered. You can also browse and share your creations with a vibrant community of AI art enthusiasts. 
+      AI Art Prompt is free to use, open-source, easy to access, and fun to explore. 
+      Start creating today and unleash your inner artist with AI Art! 🎨
+      </p>
+    </section>
   )
 }
 
