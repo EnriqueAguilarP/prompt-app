8b3f4abc6786dd409ff16efdf5894d946011badb
Jesus Aguilar <enrique.prado207@gmail.com>
2023-05-18 08:29:40 -0600

    Add SignIn and SignOut in Navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,20 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Discover and Share
+        Discover and Share 
         <br className="max-md:hidden" />
-        <span className="orange_gradient text-center">AI Art Powered Prompts</span>
+        <span className="orange_gradient text-center"> AI Art Powered Prompts </span>
       </h1>
       <p className="desc text-center">
       Welcome to AI Art Prompt, the website where you can find and share some prompts for stunning artworks in seconds using the power of Artificial Intelligence. 
-      Whether you want to turn your photo into a masterpiece, generate an artwork from nothing but words, or combine concepts, attributes, and styles in novel ways, 
-      AI Art Prompt has you covered. You can also browse and share your creations with a vibrant community of AI art enthusiasts. 
+      You can also browse and share your creations with a vibrant community of AI art enthusiasts. 
       AI Art Prompt is free to use, open-source, easy to access, and fun to explore. 
-      Start creating today and unleash your inner artist with AI Art! 🎨
+      Start creating today and unleash your inner artist with AI Art!
       </p>
+      <Feed />
     </section>
   )
 }
